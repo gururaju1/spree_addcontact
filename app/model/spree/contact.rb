ef4dd8eb81c37b554edc7c4177073ca09b0ee343
gururaju <gururaju57@gmail.com>
2016-06-22 10:30:52 +0000

    get addresses

--- app/model/spree/contact.rb
+++ app/model/spree/contact.rb
@@ -1,6 +1,6 @@
 module Spree
   class Contact < Spree::Base
-  
+  has_one :address
   has_attached_file :contactimage, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :contactimage, :content_type => /\Aimage\/.*\Z/
 end
